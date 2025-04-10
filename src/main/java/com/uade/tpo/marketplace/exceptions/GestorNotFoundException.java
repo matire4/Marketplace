fb5ff48e97e296bbd716e1c31a608a4f40804eb3
ManuelBanchero <72319812+ManuelBanchero@users.noreply.github.com>
2025-04-10 13:08:35 -0300

    Update GestorNotFoundException.java

--- src/main/java/com/uade/tpo/marketplace/exceptions/GestorNotFoundException.java
+++ src/main/java/com/uade/tpo/marketplace/exceptions/GestorNotFoundException.java
@@ -3,6 +3,6 @@ package com.uade.tpo.marketplace.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "El Gestor que estas intentando crear ya existe")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Gestor no encontrado")
 public class GestorNotFoundException extends Exception {
 }
