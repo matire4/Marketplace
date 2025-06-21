25b40c60b5b9e1e30d015d6217fcbf28a257d90a
Matias Meletto <matisilmica@gmail.com>
2025-06-20 21:50:49 -0300

    ultima

--- src/main/java/com/uade/tpo/marketplace/repository/CarritoDepartamentoRepository.java
+++ src/main/java/com/uade/tpo/marketplace/repository/CarritoDepartamentoRepository.java
@@ -3,6 +3,7 @@ package com.uade.tpo.marketplace.repository;
 import com.uade.tpo.marketplace.entities.CarritoDepartamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -12,6 +13,6 @@ public interface CarritoDepartamentoRepository extends JpaRepository<CarritoDepa
     
     boolean existsByCarritoIdAndDepartamentoId(Long carritoId, Long departamentoId);
 
-    @Query("SELECT cd FROM CarritoDepartamento cd WHERE cd.carrito.id = ?1 AND cd.departamento.id = ?2")
-    void deleteByCarritoIdAndDepartamentoId(Long carritoId, Long departamentoId);
+    @Query("SELECT cd FROM CarritoDepartamento cd WHERE cd.carrito_id = :carritoId AND cd.departamento_id = :departamentoId")
+    void deleteByCarritoIdAndDepartamentoId(@Param("carritoId") Long carritoId, @Param("departamentoId") Long departamentoId);
 }
--- src/main/java/com/uade/tpo/marketplace/repository/CarritoHabitacionRepository.java
+++ src/main/java/com/uade/tpo/marketplace/repository/CarritoHabitacionRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.uade.tpo.marketplace.entities.Carrito;
@@ -17,6 +18,6 @@ public interface CarritoHabitacionRepository extends JpaRepository<CarritoHabita
     List<CarritoHabitacion> findByCarritoId(Long carritoId);
     List<CarritoHabitacion> findByHabitacionId(Long habitacionId);
 
-    @Query("SELECT ch FROM CarritoHabitacion ch WHERE ch.carrito.id = ?1 AND ch.habitacion.id = ?2")
-    void deleteByCarritoIdAndHabitacionId(Long id, Long id2);
+    @Query("SELECT ch FROM CarritoHabitacion ch WHERE ch.carrito_id = :id AND ch.habitacion_id = :id2")
+    void deleteByCarritoIdAndHabitacionId(@Param("id") Long id, @Param("id2") Long id2);
 } 
